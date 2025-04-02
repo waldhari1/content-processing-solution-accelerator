0e49f4e46f6571797e44990686802d9998f6eab4
Prajwal D C <v-dcprajwal@microsoft.com>
2025-04-03 00:07:02 +0530

    fix: Added executable permission for shell scripts in dev container

--- .devcontainer/setupEnv.sh
+++ .devcontainer/setupEnv.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
 set -e  # Exit on error
+echo "Pull latest code for the current branch"
+git fetch
+git pull
 
 echo "Setting up ContentProcessor..."
 cd ./src/ContentProcessor
 uv sync --frozen
 cd ../../
 
-pwd
-
 echo "Setting up ContentProcessorApi..."
 cd ./src/ContentProcessorAPI
 uv sync --frozen
 cd ../../
-pwd
 
 echo "Installing dependencies for ContentProcessorWeb..."
 cd ./src/ContentProcessorWeb
 yarn install
+
+cd ../../
+
+echo "Setting up executable permission for shell scripts"
+chmod +x ./infra/scripts/docker-build.sh
+chmod +x ./src/ContentProcessorAPI/samples/upload_files.sh
+chmod +x ./src/ContentProcessorAPI/samples/schemas/register_schema.sh
 
 echo "Setup complete! 🎉"
