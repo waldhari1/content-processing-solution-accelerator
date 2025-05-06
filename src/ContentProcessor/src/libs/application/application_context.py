5dea88e3574a7d9516dd9c295998cf65eb77b57b
DB Lee <donlee@microsoft.com>
2025-05-06 19:31:27 +0000

    adding Semantic Kernel Object to AppContext object

--- src/ContentProcessor/src/libs/application/application_context.py
+++ src/ContentProcessor/src/libs/application/application_context.py
@@ -1,4 +1,6 @@
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 
 from libs.application.application_configuration import AppConfiguration
 from libs.base.application_models import AppModelBase
@@ -12,9 +14,28 @@ class AppContext(AppModelBase):
 
     configuration: AppConfiguration = None
     credential: DefaultAzureCredential = None
+    kernel: Kernel = None
 
     def set_configuration(self, configuration: AppConfiguration):
         self.configuration = configuration
 
     def set_credential(self, credential: DefaultAzureCredential):
         self.credential = credential
+
+    def set_kernel(self):
+        kernel = Kernel()
+
+        kernel.add_service(
+            AzureChatCompletion(
+                service_id="vision-agent",
+                endpoint=self.configuration.app_azure_openai_endpoint,
+                # api_key=self.app_config.azure_openai_key,
+                ad_token_provider=get_bearer_token_provider(
+                    DefaultAzureCredential(),
+                    "https://cognitiveservices.azure.com/.default",
+                ),
+                deployment_name=self.configuration.app_azure_openai_model,
+            )
+        )
+
+        self.kernel = kernel
