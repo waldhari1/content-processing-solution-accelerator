b4126dbf8ac4fd61e473c6a601a1cb233cfbc74e
Prajwal-Microsoft <v-dcprajwal@microsoft.com>
2025-05-16 09:25:49 +0530

    Revert "refactor: replace open ai sdk to semantic kernel"

--- src/ContentProcessor/src/libs/semantic_kernel_extended/custom_execution_settings.py
+++
@@ -1,5 +0,0 @@
-from semantic_kernel.connectors.ai.open_ai import AzureChatPromptExecutionSettings
-
-
-class CustomChatCompletionExecutionSettings(AzureChatPromptExecutionSettings):
-    logprobs: bool = False
